85134665e9c16a0e601ff1648870b6e655f26069
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-04 23:42:04 +0530

    initial contract

---
+++ contracts/paperhouse.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PaperHouse is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _paperIds;
+
+    constructor() ERC721("PaperHouse", "PH") {
+    }
+
+    struct ResearchPaper{
+        address owner;
+        string author;
+        uint256 tokenId;
+        bool isfunding;
+    }
+
+    mapping(uint256=>ResearchPaper) public papers;
+
+    function publish(string memory tokenURI,string memory _author,bool _isfunding) public {
+        _tokenIds.increment();
+        _paperIds.increment();
+        
+        uint256 newtokenId = _tokenIds.current();
+        uint256 newpaperId = _paperIds.current();
+        
+        _mint(msg.sender, newtokenId);
+        _setTokenURI(newtokenId, tokenURI);
+
+        ResearchPaper memory rpaper = ResearchPaper(msg.sender,_author,newtokenId,_isfunding);
+
+        papers[newpaperId]=rpaper;
+
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var PaperHouse = artifacts.require("./PaperHouse.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(PaperHouse);
 };
