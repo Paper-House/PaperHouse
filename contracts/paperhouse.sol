81cdc991e1823d8b1ddc69befe208db52a3af9c6
SairajK19 <saikapdi11@gmail.com>
2021-07-05 12:51:16 +0530

    Added getPapers function

--- contracts/paperhouse.sol
+++ contracts/paperhouse.sol
@@ -10,31 +10,50 @@ contract PaperHouse is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     Counters.Counter private _paperIds;
 
-    constructor() ERC721("PaperHouse", "PH") {
-    }
+    constructor() ERC721("PaperHouse", "PH") {}
 
-    struct ResearchPaper{
+    struct ResearchPaper {
         address owner;
         string author;
         uint256 tokenId;
         bool isfunding;
     }
 
-    mapping(uint256=>ResearchPaper) public papers;
+    mapping(uint256 => ResearchPaper) public papers;
 
-    function publish(string memory tokenURI,string memory _author,bool _isfunding) public {
+    function publish(
+        string memory tokenURI,
+        string memory _author,
+        bool _isfunding
+    ) public {
         _tokenIds.increment();
         _paperIds.increment();
-        
+
         uint256 newtokenId = _tokenIds.current();
         uint256 newpaperId = _paperIds.current();
-        
+
         _mint(msg.sender, newtokenId);
         _setTokenURI(newtokenId, tokenURI);
 
-        ResearchPaper memory rpaper = ResearchPaper(msg.sender,_author,newtokenId,_isfunding);
+        ResearchPaper memory rpaper = ResearchPaper(
+            msg.sender,
+            _author,
+            newtokenId,
+            _isfunding
+        );
 
-        papers[newpaperId]=rpaper;
+        papers[newpaperId] = rpaper;
+    }
 
+    function getPapers(uint256 paperId)
+        public
+        view
+        returns (ResearchPaper memory, string memory)
+    {
+        string memory tokenUri;
+
+        tokenUri = tokenURI(paperId);
+
+        return (papers[paperId], tokenUri);
     }
 }
