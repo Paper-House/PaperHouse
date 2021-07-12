32e3ad31ae5a39b89dc1b7bdb9e3601eaa795372
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-13 01:23:48 +0530

    added explore page

--- client/src/components/explore/Explore.js
+++ client/src/components/explore/Explore.js
@@ -1,5 +1,113 @@
 import React from "react";
+import "./explore.css";
+import { Link } from "react-router-dom";
+import PaperCard from "./PaperCard";
 
 export const Explore = () => {
-  return <div>explore</div>;
+  const data = [
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    }, {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+    },
+  ];
+  return (
+    <div className="explore container">
+      <div className="explore_title">
+        <h3>Explore</h3>
+      </div>
+      <div className="explore_categories">
+        <div className="explore_category_item">
+          <h3>All</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>⚛ Science</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>🚀 Space</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>📃 Whiite Papers</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>🤖 ML/AL</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>⚕️ Medical</h3>
+        </div>
+        <div className="explore_category_item">
+          <h3>📊 Economics</h3>
+        </div>
+      </div>
+      <div className="explore_papers">
+        {data.map((paper) => {
+          return (
+            <Link to={`paper?token=${paper.paperid}`}>
+              <PaperCard data={paper} />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
---
+++ client/src/components/explore/PaperCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { AddressBtn } from "../addressBtn";
+import thumb from "../assets/thumb.png";
+export default function PaperCard({ data }) {
+  return (
+    <div className="paper_card">
+      <div className="paper_card_img">
+        <img src={thumb} alt="" />
+      </div>
+      <div className="paper_card_details">
+        <div className="paper_card_title">
+          <h3>{data.title}</h3>
+        </div>
+        <div className="paper_card_category">{data.category}</div>
+        <div className="paper_card_date"><h3>published on {data.date}</h3></div>
+        <div className="paper_card_author">
+          <h3>{data.author}</h3>
+          <AddressBtn />
+        </div>
+      </div>
+    </div>
+  );
+}
