44af468e79f0e04288e92f62d35f1a8d77a237e6
Sanket Marathe <sanketmarathe80@gmail.com>
2021-07-13 16:09:19 +0530

    added mypapers page

--- client/src/components/mypapers/Mypapers.js
+++ client/src/components/mypapers/Mypapers.js
@@ -1,17 +1,115 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 import "./mypapers.css";
+import { Link } from "react-router-dom";
+import PaperCard from "../explore/PaperCard";
 
 export const Mypapers = () => {
+  const [category, setcategory] = useState("all");
+
+  const data = [
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "science",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "whitepapers",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "space",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "science",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "ml/ai",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "medical",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "space",
+    },
+    {
+      paperid: 1,
+      title:
+        "Computing interaction effects and standard errors in logit and probit models",
+      author: "Edward C. Norton",
+      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
+      date: "10 june 2021",
+      thumbnail: "https://ipfs",
+      category: "economics",
+    },
+  ];
   return (
-    <div className="Mypapers_container">
-      <div className="sidebar_">
-        <NavLink id="sidebar_active_button" to="/mypaper" activeClassName="active">
-          My Papers
-        </NavLink>
-        <NavLink id="my-papers" to="/activites" activeClassName="active">
-          Activities
-        </NavLink>
+    <div className="mypapers container">
+      <div className="explore_title">
+        <h3>My Papers</h3>
+      </div>
+      <div className="mypapers_papers">
+        {data.map((paper) => {
+          if (category === "all") {
+            return (
+              <Link to={`paper?token=${paper.paperid}`}>
+                <PaperCard data={paper} />
+              </Link>
+            );
+          }
+          if (paper.category === category) {
+            return (
+              <Link to={`paper?token=${paper.paperid}`}>
+                <PaperCard data={paper} />
+              </Link>
+            );
+          }
+        })}
       </div>
     </div>
   );
