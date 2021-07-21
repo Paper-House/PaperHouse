3a356c1a8e1149f18fe137351eebc30a539c8b52
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-21 17:52:14 +0530

    added reducers

---
+++ client/src/redux/reducers/papersreducer.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+let initialState = {
+  wallet: {
+    connected: false,
+    address: "",
+    network: "",
+  },
+  papers: {
+    data: [],
+  },
+  funders: {
+    data: [],
+  },
+  contract: {},
+  web3: {},
+};
+export const getpaper = createAsyncThunk("paper/getpaper", async (props) => {
+  // axios call to get all papers
+  return;
+});
+export const getfunders = createAsyncThunk(
+  "paper/getfunders",
+  async (props) => {
+    // axios call to get all funders
+    return;
+  }
+);
+
+const paperSlice = createSlice({
+  name: "paper",
+  initialState,
+  reducers: {
+    setWallet: (state, { payload }) => {
+      state.wallet.connected = payload.connected;
+      state.wallet.address = payload.address;
+      state.wallet.network = payload.network;
+    },
+    setContract: (state, { payload }) => {
+      state.contract = payload;
+    },
+    setWeb3: (state, { payload }) => {
+      state.web3 = payload;
+    },
+  },
+  extraReducers: {
+    [getpaper.pending]: (state) => {},
+    [getpaper.fulfilled]: (state, { payload }) => {
+      state.papers.data = payload;
+    },
+    [getfunders.pending]: (state) => {},
+    [getfunders.fulfilled]: (state, { payload }) => {
+      state.funders.data = payload;
+    },
+  },
+});
+
+export const { setWallet, setWeb3, setContract } = paperSlice.actions;
+export default paperSlice.reducer;
