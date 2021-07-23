367942d2ff3f918cbc139c0275b0b9f4374e2873
SairajK19 <saikapdi11@gmail.com>
2021-07-23 18:50:33 +0530

    added mobile drop down

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Hamburger from "hamburger-react";
+import { motion, useCycle } from "framer-motion";
 import "./Navbar.css";
 
 import logo from "../assets/logo.svg";
@@ -8,10 +10,24 @@ import metamask from "../assets/metamask.png";
 import portis from "../assets/portis.png";
 import ConnectWallet from "../ConnectWallet";
 
+export const useDimensions = (ref) => {
+  const dimensions = useRef({ width: 0, height: 0 });
+
+  useEffect(() => {
+    dimensions.current.width = ref.current.offsetWidth;
+    dimensions.current.height = ref.current.offsetHeight;
+  }, []);
+
+  return dimensions.current;
+};
+
 export const Navbar = () => {
+  const [isHamburgerOpen, setHamburgerOpen] = useState(false);
   const [walletToggle, setWalletToggle] = useState(false);
   const [Connecting, setConnecting] = useState(false);
   const [wallet, setwallet] = useState(0);
+  const containerRef = useRef(null);
+  const height = useDimensions(containerRef);
   const connected = useSelector((state) => state.paper.wallet.connected);
   const correctNetwork = useSelector(
     (state) => state.paper.wallet.correctNetwork
@@ -22,6 +38,7 @@ export const Navbar = () => {
     setWalletToggle(false);
     setConnecting(false);
   }, [connected]);
+
   useEffect(() => {
     if (connected && !correctNetwork) {
       state.contract.methods
@@ -31,11 +48,39 @@ export const Navbar = () => {
           console.log(data);
         });
     }
+    // window.addEventListener("", () => {
+    //   setWalletToggle(false)
+    // })
   }, [state.contract]);
+
+  const sidebar = {
+    open: (height = 1000) => ({
+      clipPath: `circle(${height / 1.8}px at 90% 30px)`,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 2,
+      },
+    }),
+    closed: {
+      clipPath: `circle(0px at 90% 35px)`,
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+
+  const hamburgerClickHandle = () => {
+    isHamburgerOpen ? setHamburgerOpen(false) : setHamburgerOpen(true);
+  };
+
   return (
     <>
       <ConnectWallet wallet={wallet} />
-      {connected?correctNetwork?null:<WrongNetwork/>:null }
+      {connected ? correctNetwork ? null : <WrongNetwork /> : null}
       <div className="nav__backdrop">
         <nav>
           <NavLink
@@ -104,20 +149,128 @@ export const Navbar = () => {
               </svg>
             </div>
             <div className="dropdown__icon--circle">
-              <svg
-                viewBox="0 0 18 8"
-                fill="none"
-                width="13.200000000000001"
-                height="13.200000000000001"
-                xlmns="http://www.w3.org/2000/svg"
+              <motion.nav
+                initial={false}
+                animate={isHamburgerOpen ? "open" : "closed"}
+                custom={height}
+                ref={containerRef}
+                className="mobile__name-motion-nav"
               >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M1 0C0.447715 0 0 0.447715 0 1C0 1.55228 0.447716 2 1 2H17C17.5523 2 18 1.55228 18 1C18 0.447715 17.5523 0 17 0H1ZM4 6C3.44772 6 3 6.44772 3 7C3 7.55228 3.44772 8 4 8H14C14.5523 8 15 7.55228 15 7C15 6.44772 14.5523 6 14 6H4Z"
-                  fill="#0097fd"
-                ></path>
-              </svg>
+                {/* <Navigation /> */}
+                <motion.div
+                  className="mobile__name-motion-div-sidebar container"
+                  variants={sidebar}
+                >
+                  <nav>
+                    <NavLink
+                      to="/"
+                      className="logo-navlink"
+                      exact
+                      activeClassName="active"
+                    >
+                      <img src={logo} />
+                    </NavLink>
+                  </nav>
+                  <div className="nav__mobile-dropdown-navigation">
+                    <div className="nav__mobile-dropdown-navigation-buttons">
+                      <NavLink
+                        id="explore"
+                        to="/explore"
+                        onClick={hamburgerClickHandle}
+                      >
+                        Explore 🚀️
+                      </NavLink>
+                      <NavLink
+                        id="my-papers"
+                        to="/myprofile"
+                        onClick={hamburgerClickHandle}
+                      >
+                        My Profile
+                      </NavLink>
+                      <div className="nav__mobile-dropdown-navigation-buttons-connect-publish">
+                        <NavLink
+                          id="publish"
+                          to="/publish"
+                          onClick={hamburgerClickHandle}
+                        >
+                          Publish
+                        </NavLink>
+                        <a
+                          id="connect"
+                          onClick={() => {
+                            window.scrollTo(0, 0);
+                            hamburgerClickHandle();
+                            setWalletToggle(!walletToggle);
+                          }}
+                        >
+                          {!connected ? "Connect" : "Connected"}
+                        </a>
+                      </div>
+                      <div className="mobile__nav-dropdown-socialMedia">
+                        <div className="mobile-nav-social">
+                          <a href="#">
+                            <svg
+                              width="18"
+                              height="18"
+                              viewBox="0 0 18 18"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                              onClick={hamburgerClickHandle}
+                            >
+                              <path
+                                d="M8.72727 0.0947266C3.90836 0.0947266 0 4.00236 0 8.822C0 12.678 2.50036 15.9493 5.96873 17.1035C6.40436 17.1842 6.54545 16.9136 6.54545 16.6838V15.0591C4.11782 15.5871 3.61236 14.0293 3.61236 14.0293C3.21527 13.0205 2.64291 12.7522 2.64291 12.7522C1.85091 12.2104 2.70327 12.222 2.70327 12.222C3.57964 12.2831 4.04073 13.1216 4.04073 13.1216C4.81891 14.4555 6.08218 14.07 6.58036 13.8467C6.65818 13.2831 6.88436 12.8976 7.13455 12.6802C5.19636 12.4584 3.15855 11.71 3.15855 8.36673C3.15855 7.41327 3.49964 6.63509 4.05745 6.02418C3.96727 5.80382 3.66836 4.91582 4.14255 3.71436C4.14255 3.71436 4.87564 3.48018 6.54327 4.60891C7.23927 4.41545 7.98545 4.31873 8.72727 4.31509C9.46909 4.31873 10.216 4.41545 10.9135 4.60891C12.5796 3.48018 13.3113 3.71436 13.3113 3.71436C13.7862 4.91655 13.4873 5.80455 13.3971 6.02418C13.9571 6.63509 14.2953 7.414 14.2953 8.36673C14.2953 11.7187 12.2538 12.4569 10.3105 12.6729C10.6233 12.9435 10.9091 13.4744 10.9091 14.2889V16.6838C10.9091 16.9158 11.0487 17.1885 11.4916 17.1027C14.9571 15.9471 17.4545 12.6765 17.4545 8.822C17.4545 4.00236 13.5469 0.0947266 8.72727 0.0947266Z"
+                                fill="white"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                        <div className="mobile-nav-social">
+                          <a href="#">
+                            <svg
+                              width="19"
+                              height="19"
+                              viewBox="0 0 19 19"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                              onClick={hamburgerClickHandle}
+                            >
+                              <path
+                                d="M9.45453 1.71698C11.8575 1.71698 12.1425 1.72598 13.092 1.76948C15.531 1.88048 16.6703 3.03773 16.7813 5.45873C16.8248 6.40748 16.833 6.69248 16.833 9.09548C16.833 11.4992 16.824 11.7835 16.7813 12.7322C16.6695 15.151 15.5333 16.3105 13.092 16.4215C12.1425 16.465 11.859 16.474 9.45453 16.474C7.05153 16.474 6.76653 16.465 5.81778 16.4215C3.37278 16.3097 2.23953 15.1472 2.12853 12.7315C2.08503 11.7827 2.07603 11.4985 2.07603 9.09473C2.07603 6.69173 2.08578 6.40748 2.12853 5.45798C2.24028 3.03773 3.37653 1.87973 5.81778 1.76873C6.76728 1.72598 7.05153 1.71698 9.45453 1.71698ZM9.45453 0.0947266C7.01028 0.0947266 6.70428 0.105227 5.74428 0.148727C2.47578 0.298727 0.659279 2.11223 0.509279 5.38373C0.465029 6.34448 0.454529 6.65048 0.454529 9.09473C0.454529 11.539 0.465029 11.8457 0.508529 12.8057C0.658529 16.0742 2.47203 17.8907 5.74353 18.0407C6.70428 18.0842 7.01028 18.0947 9.45453 18.0947C11.8988 18.0947 12.2055 18.0842 13.1655 18.0407C16.431 17.8907 18.252 16.0772 18.3998 12.8057C18.444 11.8457 18.4545 11.539 18.4545 9.09473C18.4545 6.65048 18.444 6.34448 18.4005 5.38448C18.2535 2.11898 16.4378 0.299477 13.1663 0.149477C12.2055 0.105227 11.8988 0.0947266 9.45453 0.0947266V0.0947266ZM9.45453 4.47323C6.90228 4.47323 4.83303 6.54248 4.83303 9.09473C4.83303 11.647 6.90228 13.717 9.45453 13.717C12.0068 13.717 14.076 11.6477 14.076 9.09473C14.076 6.54248 12.0068 4.47323 9.45453 4.47323ZM9.45453 12.0947C7.79778 12.0947 6.45453 10.7522 6.45453 9.09473C6.45453 7.43798 7.79778 6.09473 9.45453 6.09473C11.1113 6.09473 12.4545 7.43798 12.4545 9.09473C12.4545 10.7522 11.1113 12.0947 9.45453 12.0947ZM14.259 3.21098C13.662 3.21098 13.1783 3.69473 13.1783 4.29098C13.1783 4.88723 13.662 5.37098 14.259 5.37098C14.8553 5.37098 15.3383 4.88723 15.3383 4.29098C15.3383 3.69473 14.8553 3.21098 14.259 3.21098Z"
+                                fill="#F70075"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                        <div className="mobile-nav-social">
+                          <a href="#">
+                            <svg
+                              width="18"
+                              height="15"
+                              viewBox="0 0 18 15"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                              onClick={hamburgerClickHandle}
+                            >
+                              <path
+                                d="M17.4545 2.32255C16.8291 2.60022 16.1569 2.78722 15.4514 2.87151C16.1717 2.44014 16.7249 1.7566 16.9849 0.942012C16.3113 1.34151 15.5647 1.63193 14.7699 1.78847C14.1346 1.1106 13.2272 0.687012 12.2242 0.687012C9.9724 0.687012 8.31774 2.78793 8.82632 4.96889C5.92853 4.82368 3.3587 3.43535 1.63815 1.32522C0.724404 2.89276 1.16428 4.94339 2.71695 5.9818C2.14603 5.96339 1.6077 5.80685 1.13807 5.54547C1.09982 7.16118 2.25795 8.67276 3.93528 9.00922C3.4444 9.14239 2.90678 9.17355 2.35995 9.06872C2.80336 10.4542 4.09111 11.4622 5.61828 11.4905C4.15203 12.6401 2.3047 13.1537 0.454529 12.9355C1.99799 13.9251 3.83186 14.5023 5.80103 14.5023C12.2766 14.5023 15.9352 9.0333 15.7142 4.1281C16.3956 3.6358 16.987 3.02168 17.4545 2.32255Z"
+                                fill="#1DA1F2"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </motion.div>
+                <Hamburger
+                  toggled={isHamburgerOpen}
+                  toggle={setHamburgerOpen}
+                  color={"hsla(204, 100%, 50%, 0.733)"}
+                  direction="right"
+                  size={25}
+                  className="react-ham"
+                />
+              </motion.nav>
             </div>
           </div>
         </nav>
