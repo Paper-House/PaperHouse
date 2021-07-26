fa40b8a5f89b434a092de1673851cb38135daa94
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-27 01:18:34 +0530

    helper function to get ipfs url

--- client/src/components/explore/Explore.js
+++ client/src/components/explore/Explore.js
@@ -2,92 +2,11 @@ import React, { useState } from "react";
 import "./explore.css";
 import { Link } from "react-router-dom";
 import PaperCard from "./PaperCard";
+import { useSelector } from "react-redux";
 
 export const Explore = () => {
   const [category, setcategory] = useState("all");
-
-  const data = [
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "science",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "whitepapers",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "space",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "science",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "ml/ai",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "medical",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "space",
-    },
-    {
-      paperid: 1,
-      title:
-        "Computing interaction effects and standard errors in logit and probit models",
-      author: "Edward C. Norton",
-      publisher: "0x0aa121493Ba3f231570dBB3aAA62a9De64F374f6",
-      date: "10 june 2021",
-      thumbnail: "https://ipfs",
-      category: "economics",
-    },
-  ];
+  const papers = useSelector((state) => state.paper.papers.data);
   return (
     <div className="explore container">
       <div className="explore_title">
@@ -157,22 +76,24 @@ export const Explore = () => {
         </button>
       </div>
       <div className="explore_papers">
-        {data.map((paper) => {
-          if (category === "all") {
-            return (
-              <Link to={`/paper?token=${paper.paperid}`}>
-                <PaperCard data={paper} />
-              </Link>
-            );
-          }
-          if (paper.category === category) {
-            return (
-              <Link to={`paper?token=${paper.paperid}`}>
-                <PaperCard data={paper} />
-              </Link>
-            );
-          }
-        })}
+        {papers.length > 0
+          ? papers.map((paper) => {
+              if (category === "all") {
+                return (
+                  <Link to={`/paper/${paper.paperid}`}>
+                    <PaperCard data={paper} />
+                  </Link>
+                );
+              }
+              if (paper.category === category) {
+                return (
+                  <Link to={`/paper/${paper.paperid}`}>
+                    <PaperCard data={paper} />
+                  </Link>
+                );
+              }
+            })
+          : ""}
       </div>
     </div>
   );
---
+++ client/src/utils/getURL.js
@@ -0,0 +1,6 @@
+import { toGatewayURL } from "nft.storage";
+
+export const getURL = (url) => {
+  const tokenURI = toGatewayURL(url, (Option = { gateway: "https://ipfs.io" }));
+  return tokenURI.href;
+};
