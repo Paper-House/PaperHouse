0c229c659237effd26b1007428a17d60abe7d004
SairajK19 <saikapdi11@gmail.com>
2021-07-27 21:07:15 +0530

    added activities loading animation

--- client/src/components/activities/Activities.js
+++ client/src/components/activities/Activities.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 import "./Activities.css";
 
@@ -23,9 +24,22 @@ const ActivityCard = ({ image, title, address, fundAmount }) => {
   );
 };
 
+const ActivityCardLoading = () => {
+  return (
+    <div className="Activities__main--card" style={{padding: "0px 3px 3px 3px", display:"block", overflow: "hidden"}}>
+      {/* <img src={image} alt="thumbnail" /> */}
+      <Skeleton width={"100%"} height={"70px"} />
+    </div>
+  );
+};
+
 const Activities = () => {
   const myActivities = useSelector((state) => state.paper.myActivities).data;
   const { address } = useSelector((state) => state.paper.wallet);
+  const activityLoading = useSelector(
+    (state) => state.paper.myActivities.loading
+  );
+  // const activityLoading = true;
   const [urlAddress, setUrlAddress] = useState(null);
 
   useEffect(() => {
@@ -33,26 +47,38 @@ const Activities = () => {
   });
 
   return (
-    <div className="Activities__container">
-      <div className="Activities__main--section">
-        {myActivities.length != 0
-          ? myActivities.map((activity) => {
-              console.log(activity.title);
-              return (
-                <ActivityCard
-                  image={activity.thumbnail}
-                  title={activity.title}
-                  address={address}
-                  fundAmount={activity.amount}
-                />
-              );
-            })
-          : !address
-          ? "Connect Your Wallet"
-          : "Loading"}
-      </div>
-      <div></div>
-    </div>
+    <>
+      {!activityLoading ? (
+        <div className="Activities__container">
+          <div className="Activities__main--section">
+            {myActivities.length != 0
+              ? myActivities.map((activity) => {
+                  console.log(activity.title);
+                  return (
+                    <ActivityCard
+                      image={activity.thumbnail}
+                      title={activity.title}
+                      address={address}
+                      fundAmount={activity.amount}
+                    />
+                  );
+                })
+              : !address
+              ? "Connect Your Wallet"
+              : "Loading"}
+          </div>
+          <div></div>
+        </div>
+      ) : (
+        <div className="Activities__container">
+          <div className="Activities__main--section">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((_) => {
+              return <ActivityCardLoading />;
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
