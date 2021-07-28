6a13edbdb06cd8c6c5e4c848ba1bd0f3dc413d36
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-28 23:36:07 +0530

    code cleanup

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -177,20 +177,12 @@ export const Navbar = () => {
   }, [address]);
 
   useEffect(() => {
-    if (connected && correctNetwork) {
-      state.contract.methods
-        .name()
-        .call()
-        .then((data) => {
-          console.log(data);
-        });
-    }
     window.addEventListener("resize", () => {
       window.innerHeight >= 1024 ? setDropHeight(2.5) : setDropHeight(2.2);
       setHamburgerOpen(false);
     });
     window.innerHeight >= 1024 ? setDropHeight(2.5) : setDropHeight(2.2);
-  }, [state.contract]);
+  }, []);
 
   useEffect(() => {
     if (walletToggle) {
