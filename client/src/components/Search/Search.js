9589fbb147760b8c6d17391ab82b393f8e4fce85
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-29 00:26:48 +0530

    search engine

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,6 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import "./search.css";
 
 export default function Search({ input }) {
+  const papers = useSelector((state) => state.paper.papers.data);
+  const [result, setresult] = useState([]);
+  useEffect(() => {
+    papers.map((paper,index) => {
+      console.log(paper.title.search(input),index);
+    });
+  }, [input]);
   return <div className="nav_search_container">You searched :{input}</div>;
 }
