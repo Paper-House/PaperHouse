b5d0f4c055fe5569c1c0a540e8974732614cb5ca
c0deb1ind31 <rhutikparab1@gmail.com>
2021-07-29 13:32:09 +0530

    minor fix

--- client/src/components/paper/Paper.js
+++ client/src/components/paper/Paper.js
@@ -118,7 +118,6 @@ export const Paper = (props) => {
     }, 1500);
   }
   function FundPaper(paper_id, fundAmount) {
-    console.log(typeof fundAmount);
     if (connected && correctNetwork && paper_id && fundAmount) {
       setloading(true);
       contract.methods
@@ -128,7 +127,6 @@ export const Paper = (props) => {
           value: Web3.utils.toWei(String(fundAmount), "ether"),
         })
         .then(async (receipt) => {
-          console.log(receipt);
           setloading(false);
           toast("Research Paper Funded", toastStyles.default);
           setmaticconversion(0);
@@ -447,25 +445,29 @@ export const Paper = (props) => {
                         );
                       })
                     : null}
-                  {UiLoading
-                    ? null
-                    : PaperData.fundings.map((fund) => {
-                        return (
-                          <div className="paper_funder">
-                            <img src={pf} alt="" />
-                            <div className="paper_funder-address">
-                              {fund.from}
-                              <h2>
-                                Funded{" "}
-                                {Number(
-                                  web3.utils.fromWei(fund.amount, "ether")
-                                ).toFixed(3)}
-                                Matic
-                              </h2>
-                            </div>
+                  {UiLoading ? null : PaperData.fundings.length != 0 ? (
+                    PaperData.fundings.map((fund) => {
+                      return (
+                        <div className="paper_funder">
+                          <img src={pf} alt="" />
+                          <div className="paper_funder-address">
+                            {fund.from}
+                            <h2>
+                              Funded{" "}
+                              {Number(
+                                web3.utils.fromWei(fund.amount, "ether")
+                              ).toFixed(4)}
+                              Matic
+                            </h2>
                           </div>
-                        );
-                      })}
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div className="paper_nofunding">
+                      <h3>No Funders</h3>
+                    </div>
+                  )}
                 </div>
               </div>
             ) : null}
@@ -514,24 +516,33 @@ export const Paper = (props) => {
                 <div className="paper_fund_bar_bg"></div>
                 <div
                   className="paper_fund_bar_current"
-                  style={{
-                    width: `${
-                      (PaperData.paper.totalAmountFunded /
-                        PaperData.paper.fundAmount) *
-                      100
-                    }%`,
-                  }}
+                  style={
+                    PaperData.paper.fundAmount != "0"
+                      ? {
+                          width: `${
+                            (Number(PaperData.paper.totalAmountFunded) /
+                              Number(PaperData.paper.fundAmount)) *
+                            100
+                          }%`,
+                        }
+                      : { width: "0px" }
+                  }
                 ></div>
                 <h3>
-                  {(
-                    (PaperData.paper.totalAmountFunded /
-                      PaperData.paper.fundAmount) *
-                    100
-                  ).toFixed(2)}
+                  {PaperData.paper.fundAmount != "0"
+                    ? (
+                        (Number(PaperData.paper.totalAmountFunded) /
+                          Number(PaperData.paper.fundAmount)) *
+                        100
+                      ).toFixed(2)
+                    : "0"}
                   %
                 </h3>
               </div>
-              <button onClick={() => setShowPopup(!ShowPopup)}>
+              <button
+                disabled={(PaperData.paper.fundAmount == "0") == true}
+                onClick={() => setShowPopup(!ShowPopup)}
+              >
                 FUND THIS RESEARCH PAPER
               </button>
             </div>
