ba47030bf71720ac92d510d4a43dfca3e27faf51
SairajK19 <saikapdi11@gmail.com>
2021-08-08 16:47:39 +0530

    removed timer

--- client/src/App.js
+++ client/src/App.js
@@ -18,17 +18,17 @@ export default function App() {
   return (
     <Router>
       <SkeletonTheme color="#1e5383" highlightColor="rgba(0, 151, 253,.4)">
-        {/* <Navbar /> */}
+        <Navbar />
         <Switch>
-          <Route path="/" component={CountdownTimer} />
-          {/* <Route path="/explore" component={Explore} />
+          <Route path="/" exact component={Explore} />
+          <Route path="/explore" component={Explore} />
           <Route path="/publish" component={Publish} />
           <Route path="/mypaper" component={Mypapers} />
           <Route path="/paper/:paperid" component={Paper} />
           <Route path="/myprofile" component={Profile} />
           <Route path="/profile" component={ProfilePapers} />
           <Route path="/not-found" component={NotFound} />
-          <Route component={NotFound} /> */}
+          <Route component={NotFound} />
         </Switch>
       </SkeletonTheme>
     </Router>
